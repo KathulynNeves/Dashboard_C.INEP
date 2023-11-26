6e68c915a558f3abbe24faa0bdb00d32b1f4e6ed
bi-nho <131626706+bi-nho@users.noreply.github.com>
2023-11-25 22:53:02 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 var menubut = document.getElementById("menubut")
 var menu = document.getElementById("menu")
+var filtros = document.getElementById("filtros")
 
 menubut.addEventListener("click", function(){
     if(menu.style.display === "block"){
         menu.style.display = "none"
     } else {
         menu.style.display = "block"
+    }
+})
+
+menubut.addEventListener("click", function(){
+    if(filtros.style.display === "none"){
+        filtros.style.display = "block"
+    } else {
+        filtros.style.display = "none"
     }
 })
